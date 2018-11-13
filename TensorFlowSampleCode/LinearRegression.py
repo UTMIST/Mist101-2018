ed50a6fb1973da31b722ba2fac008209c30cd275
Yuchen Wu <cheneyuwu@gmail.com>
2018-11-12 22:57:48 -0500

    Modified LR

--- TensorFlowSampleCode/LinearRegression.py
+++ TensorFlowSampleCode/LinearRegression.py
@@ -5,17 +5,40 @@ To run this script:
 Check optional arguments:
     - python LinearRegression.py -h
 """
-
+# System Import
 import os
 import argparse
 import sys
-
+# Tensorflow
+import tensorflow as tf
+# Other dependencies
 import numpy as np
 import matplotlib.pyplot as plt
-import tensorflow as tf
 
 # Used for argument parse
 FLAGS = None
+
+class LinearModel:
+
+    def __init__(self):
+        with tf.name_scope('Model/'):
+            self.w = tf.Variable(0.0, name="Weights")
+            self.b = tf.Variable(0.0, name="Bias")
+
+    def forward(self, x):
+        with tf.name_scope('Model/'):
+            output = self.w * x + self.b
+        return output
+
+def compute_loss(prediction_y, label_y):
+    with tf.name_scope('Loss'):
+        loss = tf.reduce_sum(tf.squared_difference(label_y, prediction_y))/(2*FLAGS.num_data)
+    return loss
+    
+def train(loss, learning_rate = 0.0001):
+    with tf.name_scope('Gradient'):
+        gradient = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
+    return gradient
 
 # Generating artificial data.
 def generate_fake_data(n=50):
@@ -24,42 +47,10 @@ def generate_fake_data(n=50):
     :param n: Number of fake data.
     :return: Fake data.
     """
-    XX = np.arange(n)
-    YY = np.random.randint(-10, 10, size=50) + 2.0 * XX
-    data = np.stack([XX,YY], axis=1)
+    feature_x = np.arange(n)
+    label_y = np.random.randint(-10, 10, size=50) + 2.0 * feature_x
+    data = np.stack([feature_x,label_y], axis=1)
     return data
-
-def linear_model(X):
-    """
-    Forward passing the X.
-    :param X: Input.
-    :return: X*W + b.
-    """
-    # creating the weight and bias.
-    # The defined variables will be initialized to zero.
-    with tf.name_scope('model'):
-        W = tf.Variable(0.0, name="weights")
-        b = tf.Variable(0.0, name="bias")
-        output = X * W + b
-    return output
-
-def square_loss(Y_predict, Y, data):
-    """
-    compute the loss by comparing the predicted value to the actual label.
-    :param X: The output from model.
-    :param Y: The label.
-    :return: The loss over the samples.
-    """
-    with tf.name_scope('loss'):
-        loss = tf.reduce_sum(tf.squared_difference(Y, Y_predict))/(2*data.shape[0])
-    return loss
-
-# The training function.
-def train(loss, learning_rate = 0.0001):
-    with tf.name_scope('gradient'):
-        gradient = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss)
-    return gradient
-
 
 def plot(data, wcoeff, bias):
     input_values = data[:,0]
@@ -81,13 +72,13 @@ def main(_):
     # build graph
     graph = tf.Graph()
     with graph.as_default():
-        # input data
-        X = tf.placeholder(tf.float32, name="X")
-        Y = tf.placeholder(tf.float32, name="Y")
+        x = tf.placeholder(tf.float32, name="X")
+        y = tf.placeholder(tf.float32, name="Y")
+        model = LinearModel()
         # create graph: model (inference)
-        Y_predict = linear_model(X)
+        prediction_y = model.forward(x)
         # create graph: loss function
-        train_loss = square_loss(Y_predict, Y, data)
+        train_loss = compute_loss(prediction_y, y)
         # create graph: train
         train_op = train(train_loss, FLAGS.learning_rate)
     # training
@@ -97,18 +88,19 @@ def main(_):
         # train the model
         for epoch_num in range(FLAGS.num_epoch):
             loss_value, _ = sess.run([train_loss,train_op],
-                                    feed_dict={X: data[:,0], Y: data[:,1]})
+                                    feed_dict={x: data[:,0], y: data[:,1]})
             # Displaying the loss per epoch.
             print('epoch %d, loss=%f' %(epoch_num+1, loss_value))
         # save the values of weight and bias TODO
-        # wcoeff, bias = sess.run([W, b])
+        wcoeff = sess.run([model.w])
+        bias = sess.run([model.b])
         try:
             writer = tf.summary.FileWriter(FLAGS.log_dir, sess.graph)
             writer.close()
         except:
             print("Errorr: Change your directory!")
-    # plot the result
-    # plot(data, wcoeff, bias)
+    # Plot the result.
+    plot(data, wcoeff, bias)
     return 0
 
 
